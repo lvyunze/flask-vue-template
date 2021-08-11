10c54adc88233fbabc2fe9f88fe308ee4f42fa3a
Jack <37875663+lvyunze@users.noreply.github.com>
2021-08-11 10:39:53 +0800

    添加日志使用设置

--- backend/run.py
+++ backend/run.py
@@ -2,12 +2,18 @@ from flask_jwt_extended import JWTManager
 import logging
 from flask_cors import CORS
 from apps.routers import create_app
+from logging.handlers import TimedRotatingFileHandler
 app = create_app()
 
 if __name__ == '__main__':
     app.debug = True
-    handler = logging.FileHandler('flask.log')
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s")
+    handler = TimedRotatingFileHandler(
+        "flask.log", when="D", interval=1, backupCount=15,
+        encoding="UTF-8", delay=False, utc=True)
     app.logger.addHandler(handler)
+    handler.setFormatter(formatter)
     CORS(app)
     jwt = JWTManager(app)
     app.run()
